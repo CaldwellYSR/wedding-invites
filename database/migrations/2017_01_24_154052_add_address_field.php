e5602ddc8c2de54b097ab301ab13091242959130
Matthew Caldwell <caldwellysr@gmail.com>
2017-01-24 10:51:15 -0500

    add address field

---
+++ database/migrations/2017_01_24_154052_add_address_field.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAddressField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('invites', function (Blueprint $table) {
+            $table->string('address')->nullable()->after('attending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('invites', function (Blueprint $table) {
+            $table->dropColumn('address');
+        });
+    }
+}
