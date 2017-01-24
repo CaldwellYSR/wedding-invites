33dad7fc7115e6bc04086270f09eed903acfab7e
Matthew Caldwell <caldwellysr@gmail.com>
2017-01-24 13:24:17 +0000

    initial commit

---
+++ app/Http/Controllers/InviteController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Invite;
+
+class InviteController extends Controller
+{
+    /**
+     * @method getInvitee - Check database for existing confirmation code and return the data related to that invitee
+     * @param [Request] $request - The GET request is passed to the function
+     */
+    public function getInvitee(Request $request)
+    {
+        $code = $request->input("confirmationCode");
+        if ($code) {
+            $invite = Invite::where('confirmationCode', $code)->firstOrFail();
+            return $invite;
+        } 
+        return null;
+    }
+    
+    /**
+     * @method rsvp - Update Invite model with RSVP information. 
+     */
+    public function rsvp(Request $request) 
+    {
+        $code = $request->input("confirmationCode");
+        if ($code) {
+            $invite = Invite::where('confirmationCode', $code)->firstOrFail();
+            $this->updateRSVP($invite, $request->all());
+        }
+    }
+     
+    private function updateRSVP($invitee, $data) {
+        $invitee->name = $data["name"];
+        $invitee->email = $data["email"];
+        $invitee->attending = $data["attending"];
+        $invitee->plusOne = $data["plusOne"];
+        $invitee->plusOneName = $data["plusOneName"];
+        $invitee->message = $data["message"];
+        $invitee->save();
+    }
+     
+}
---
+++ database/migrations/2017_01_23_181021_create_invites_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->integer('confirmationCode')->unique();
+            $table->boolean('attending')->default(false);
+            $table->boolean('plusOne')->default(false);
+            $table->string('plusOneName')->nullable();
+            $table->string('message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invites');
+    }
+}
+
+
+
